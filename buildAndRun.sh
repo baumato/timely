6df04728e352147207500bdbbedf002f4337d002
Tobias Baumann <tobbaumann@gmail.com>
2018-04-29 01:52:07 +0200

    echo container id

--- buildAndRun.sh
+++ buildAndRun.sh
@@ -5,3 +5,5 @@ docker build -t de.baumato/timely .
 
 # run the container
 docker rm -f timely || true && container=$(docker run -d -p 9080:9080 -p 9443:9443 --name timely de.baumato/timely) 
+
+echo $container
