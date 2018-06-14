cbab8617cf79a0bd9f6169da316e0043d4df4af9
Tobias Baumann <tobbaumann@gmail.com>
2018-06-14 22:00:45 +0200

    format

--- buildAndRun.sh
+++ buildAndRun.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
 # do the buid with maven inside docker
-docker build -t de.baumato/timely .
+docker image build -t de.baumato/timely .
 
 # run the container
-docker rm -f timely || true && container=$(docker run -d -p 9080:9080 -p 9443:9443 --name timely de.baumato/timely) 
+docker rm -f timely || true && \
 
-echo $container
+# how to mount a volume
+#  --mount source=mvn-repo,target=/usr/share/mvn-repo \
+
+docker run -d \
+  -p 9080:9080 \
+  -p 9443:9443 \
+  --name timely \
+  de.baumato/timely \
