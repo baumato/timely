34eadcd85f7ffa921aae1719c0b10cbd1398e1ad
Tobias Baumann <tobbaumann@gmail.com>
2018-06-19 21:02:56 +0200

    Remove comment

--- buildAndRun.sh
+++ buildAndRun.sh
@@ -5,10 +5,6 @@ docker image build -t de.baumato/timely .
 
 # run the container
 docker rm -f timely || true && \
-
-# how to mount a volume
-#  --mount source=mvn-repo,target=/usr/share/mvn-repo \
-
 docker run -d \
   -p 9080:9080 \
   -p 9443:9443 \
